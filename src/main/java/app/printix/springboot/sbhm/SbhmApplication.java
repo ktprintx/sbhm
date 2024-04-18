1048ead490ea5c0626d53d96165458d3cdf99cf6
Konstantin Tchoumak <Konstantin.Tchoumak@tungstenautomation.com>
2024-04-18 16:35:51 -0400

    Initial project from: https://www.youtube.com/watch?v=mJtEmL5F0DU&list=PLA7e3zmT6XQXIFcweUahWz2rQtmeI8dnl https://start.spring.io/

---
+++ src/main/java/app/printix/springboot/sbhm/SbhmApplication.java
@@ -0,0 +1,13 @@
+package app.printix.springboot.sbhm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbhmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbhmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/printix/springboot/sbhm/SbhmApplicationTests.java
@@ -0,0 +1,13 @@
+package app.printix.springboot.sbhm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbhmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
